66eb230839119e9ef76a6647dc433eb5dbf79f81
JoaoTS20 <joaots@ua.pt>
2021-06-29 18:02:24 +0100

    Routing main components done

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { RouterModule,Routes } from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {AllteamsComponent} from "./allteams/allteams.component";
 import {OneteamComponent} from "./oneteam/oneteam.component";
+import {AllCompetitionsComponent} from "./all-competitions/all-competitions.component";
+import {AllStaffComponent} from "./all-staff/all-staff.component";
+import {AllPlayersComponent} from "./all-players/all-players.component";
 
 const routes: Routes =[
-  {path: '', component: HomepageComponent}
+  {path: '', component: HomepageComponent},
+  {path: 'competitions', component: AllCompetitionsComponent},
+  {path: 'players', component: AllPlayersComponent},
+  {path: 'staff', component: AllStaffComponent},
+  {path: 'teams', component:AllteamsComponent}
+
 ]
 
 
