dc9116be0afdd8b21f4016d6c065ed976d03dcad
JoaoTS20 <joaots@ua.pt>
2021-06-30 18:01:56 +0100

    Commit

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthServiceService} from "./auth-service.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user: string | null | undefined;
+  userID: string | null | undefined;
+
+  constructor(private authServiceService: AuthServiceService) {
+  }
+  ngOnInit(): void {
+    this.user = localStorage.getItem('username')
+    this.userID = localStorage.getItem('userID')
+    console.log(localStorage.getItem('username'))
+    console.log(localStorage.getItem('userID'))
+  }
+
+  logout(): void{
+    this.authServiceService.logout();
+    this.ngOnInit();
+  }
+
   title = 'AngularClient';
 }
